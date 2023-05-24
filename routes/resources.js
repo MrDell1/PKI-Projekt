1b044b0e0d2e95780e373a6b28c4785313d2a1e9
Marcin Leszczyński <leszczynski-marcin@wp.pl>
2023-05-24 09:16:28 +0200

    add new endpoints

--- routes/resources.js
+++ routes/resources.js
@@ -3,14 +3,54 @@ var router = express.Router();
 const jwt = require("jsonwebtoken");
 let connection = require("../database").databaseConnection;
 
-router.get("/public", function (req, res) {
-  connection.query("SELECT data FROM publicdata", (err, result) => {
+router.get("/databaseName", function (req, res) {
+  connection.query("SELECT DATABASE() AS `Name`", (err, result) => {
     if (err) {
+      console.log(err);
       return res.status(400).send({ error: err });
     }
+    console.log(result[0].Name);
     return res.status(200).send({
-      user: result,
+      name: result[0].Name,
     });
+  });
+});
+
+router.get("/tablesNames", function (req, res) {
+  connection.query(
+    "SELECT TABLE_NAME AS `tableName` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = DATABASE()",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).send({ error: err });
+      }
+      console.log(result);
+      return res.status(200).send(result);
+    }
+  );
+});
+
+router.get("/tableValue", function (req, res) {
+  const tableName = req.query.tableName;
+  connection.query(`SELECT * FROM ${tableName}`, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).send({ error: err.sqlMessage });
+    }
+    console.log(result);
+    return res.status(200).send(result);
+  });
+});
+
+router.get("/SQLRequest", function (req, res) {
+  const request = req.query.request;
+  connection.query(`${request}`, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).send({ error: err.sqlMessage });
+    }
+    console.log(result);
+    return res.status(200).send(result);
   });
 });
 
